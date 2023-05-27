06b0ee80af627875af9f046b41a687206df392bd
flaviadanielareisenauer <flaviareisenauer@gmail.com>
2023-05-27 20:35:14 -0300

    first commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,9 @@
+type Props = { children: React.ReactNode };
+
+export default function RootLayout({ children }: Props) {
+  return (
+    <html lang="es">
+      <body>{children}</body>
+    </html>
+  );
+}
